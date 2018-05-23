6745c0ec6049b13f48975dc6310d0f6e68e066b4
suthee <bsuthee@gmail.com>
2018-05-23 17:00:00 +0700

    router

---
+++ pages/register.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>
+            Register (ลงทะเบียน)
+        </h1>
+        <v-text-field v-model="user" label="UserName" />
+        <v-text-field v-model="pass" type="password" label="Password" />
+        <v-text-field v-model="pass2" type="password" label="Password Again" />
+        <v-btn label="Regis" @click="register"><v-icon>track_changes</v-icon>Register</v-btn> 
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            user:'',
+            pass: '',
+            pass2: '',
+        }
+    },
+    methods: {
+        registor(){
+            
+        }
+
+    }
+}
+</script>
+
