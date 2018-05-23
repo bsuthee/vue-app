274143af27bdbd95b61ba5d9e7b9f3795983b5cf
suthee <bsuthee@gmail.com>
2018-05-23 15:30:39 +0700

    router

---
+++ pages/about-day1-13.20.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+   <h1> About {{name.toUpperCase()}} {{surname}} </h1>
+   <h2> age {{age * 2}} </h2>
+   <input type="text" :value="name" > <br>
+   <input type="text" v-model="name"> <br>
+   <input type="text" v-model="name">
+   </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      name: 'Suthee',
+      age: 25,
+      surname: 'Banprasert'
+    }
+  }
+}
+</script>
+
